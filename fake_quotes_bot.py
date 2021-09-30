a870fcc89a0940c7e4539022fd2de639d6cf12a0
IanDierckx <ian.dierckx@student.uantwerpen.be>
2021-09-30 22:16:12 +0200

    Bot will now also reply to comments with quotes

--- fake_quotes_bot.py
+++ fake_quotes_bot.py
@@ -18,11 +18,18 @@ def run_bot():
             posts_replied_to = list(filter(None, posts_replied_to))
 
     for submission in subreddit.new(limit=10):
+        for comment in submission.comments:
+            if comment.id not in posts_replied_to:
+                if re.search("[\"\'][\w \.]*[\"\'] - [\w \.]*", comment.body, re.IGNORECASE):
+                    comment.reply("\"Fake Quote on comment.\" - Mrs. Test")
+                    print("Bot replying to comment: ", comment.body, "by ", comment.author)
+                    posts_replied_to.append(comment.id)
         if submission.id not in posts_replied_to:
             if re.search("[\"\'][\w \.]*[\"\'] - [\w \.]*", submission.title, re.IGNORECASE):
-                submission.reply("\"Fake Quote\" - Mrs. Test")
-                print("Bot replying to : ", submission.title)
+                reply = submission.reply("\"Fake Quote on post.\" - Mrs. Test")
+                print("Bot replying to post: ", submission.title, "by ", submission.author)
                 posts_replied_to.append(submission.id)
+                posts_replied_to.append(reply.id)
 
     with open("posts_replied_to.txt", "w") as file:
         for post_id in posts_replied_to:
